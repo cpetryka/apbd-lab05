d01bcfd1e49911952bd2d3262ae2c03338c87399
Cezary Petryka <cezaryp10@gmail.com>
2024-04-14 21:52:34 +0200

    Add Id field to a VetVisit class

--- Tutorial5/Tutorial5/Model/VetVisit.cs
+++ Tutorial5/Tutorial5/Model/VetVisit.cs
@@ -2,6 +2,7 @@ namespace Tutorial5.Model;
 
 public class VetVisit
 {
+    public int Id { get; set; }
     public DateTime Date { get; set; }
     public Animal Animal { get; set; }
     public string Description { get; set; }

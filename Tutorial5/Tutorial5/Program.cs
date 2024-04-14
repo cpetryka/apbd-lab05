7e4c97f9c53f6348854d821dd439f66d82f52ef3
Cezary Petryka <cezaryp10@gmail.com>
2024-04-14 21:44:48 +0200

    Register animal endpoints

--- Tutorial5/Tutorial5/Program.cs
+++ Tutorial5/Tutorial5/Program.cs
@@ -1,3 +1,5 @@
+using Tutorial5.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,5 +17,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapAnimalEndpoints();
 
 app.Run();

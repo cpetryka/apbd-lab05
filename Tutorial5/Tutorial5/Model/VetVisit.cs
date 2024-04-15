1ba9ee4751ab5d1daed1380d0bcb350097b3ae72
Cezary Petryka <cezaryp10@gmail.com>
2024-04-15 08:22:30 +0200

    Add all args constructor to VetVisit class

--- Tutorial5/Tutorial5/Model/VetVisit.cs
+++ Tutorial5/Tutorial5/Model/VetVisit.cs
@@ -7,4 +7,13 @@ public class VetVisit
     public Animal Animal { get; set; }
     public string Description { get; set; }
     public double Price { get; set; }
+
+    public VetVisit(int id, DateTime date, Animal animal, string description, double price)
+    {
+        Id = id;
+        Date = date;
+        Animal = animal;
+        Description = description;
+        Price = price;
+    }
 }
